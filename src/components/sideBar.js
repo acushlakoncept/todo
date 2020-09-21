22a972853ccfed6f663c5ae496a23188748efb03
Uduak Essien <acushla4real@gmail.com>
2020-09-21 16:31:23 +0100

    add sidebar content/links

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -13,11 +13,43 @@ const hamburger = () => {
   bars.classList.add('fas', 'fa-bars', 'toggle-nav');
 };
 
+const createTask = () => (
+  '<button class="btn btn-primary mt-4 mb-4 text-center">Create Task</button>'
+);
+
+const quickLinks = () => (
+  `<div class="quick-links">
+      <h2 class="border-bottom pb-2 title mt-4">Quick Links</h2>
+      <nav class="nav flex-column">
+        <a class="nav-link" href="#">All Task</a>
+        <a class="nav-link" href="#">Today</a>
+        <a class="nav-link" href="#">Next 7 Days</a>
+      </nav>
+  </div>
+  `
+)
+
+const lists = () => (
+  `<div class="quick-links">
+      <h2 class="border-bottom pb-2 title mt-4">Lists</h2>
+      <nav class="nav flex-column">
+        <a class="nav-link" href="#">Personal <span class="badge badge-light">1</span> </a>
+        <a class="nav-link" href="#">Work <span class="badge badge-light">4</span> </a>
+        <a class="nav-link" href="#">Grocery <span class="badge badge-light">2</span> </a>
+      </nav>
+  </div>
+  `
+)
+
 const header = () => (
-  `<div class="d-flex align-items-center justify-content-between flex-wrap">
+  `<div class="d-flex align-items-center justify-content-between flex-wrap border-bottom pb-2">
     <i class="fas fa-bars toggle-nav"></i>
-    <h2>My TODO</h2>
-  </div>`
+    <h2 class="title">My TODO</h2>
+  </div>
+  ${createTask()}
+  ${quickLinks()}
+  ${lists()}
+  `
 );
 
 export default header;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import sideBar from './components/sideBar';
 
 const mainPage = () => (
   `<div class="row">
-    <div class="col-1 bg-dark p-5 side-bar">
+    <div class="col-2 p-5 side-bar">
       ${sideBar()}
     </div>
-    <div class="col-11 bg-light p-5 main">
+    <div class="col-10 bg-light p-5 main">
 
     </div>
   </div>`
